03987b3f4280bf8886a732ec577a9e63bcba0b83
prashantu4040 <prashantudale@gmail.com>
2021-12-16 11:26:11 +0530

    Created BMI Calculaor

---
+++ script.js
@@ -0,0 +1,28 @@
+function bmi() {
+    // get feet & inches value
+    let feet = document.querySelector(".feet").value;
+    let inch = document.querySelector(".inch").value;
+    // Merge feet & inches value in one
+    let metre = feet + "." + inch;
+    // feet to metre conversation & get the height in metre
+    let height = metre * 0.3048;
+    // get weight value
+    let weight = document.querySelector(".weight").value;
+    // BMI Calculation
+    let bmi = weight / Math.pow(height, 2);
+    // BMI showing in p tag with condition
+    if(bmi<18.5){
+        document.querySelector("p").innerHTML = "Your Body Mass Index(BMI) is:- "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is Under Weight";
+    }
+    else if(bmi>=18.5 && bmi<25){
+        document.querySelector("p").innerHTML = "Your Body Mass Index(BMI) is:- "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is Normal & It's Healthy";
+    }
+    else if(bmi>=25 && bmi<30){
+        document.querySelector("p").innerHTML = "Your Body Mass Index(BMI) is:- "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is in over Weight";
+    }
+    else if(bmi>30){
+        document.querySelector("p").innerHTML = "Your Body Mass Index(BMI) is:- "+(Math.round(bmi*100)/100).toFixed(2)+"<br>"+"Index Value is in Obesity";
+    }
+}
+// Click event fire on calculate button
+document.querySelector(".btn").addEventListener("click", bmi);
